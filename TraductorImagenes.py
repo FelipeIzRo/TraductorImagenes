13e7c2d2e9929b9bac228bdcff58c4f1ae1ef07e
FelipeIzRo <felipe.iz.ro.98@gmail.com>
2024-07-28 17:06:15 +0200

    Estructura de clase POO

--- TraductorImagenes.py
+++ TraductorImagenes.py
@@ -7,18 +7,24 @@ from googletrans import Translator
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 # Example tesseract_cmd = r'C:\Program Files (x86)\Tesseract-OCR\tesseract'
 
-text = pytesseract.image_to_string(Image.open('test.png'))
-if text:
-    print("TEXTO RECOGIDO DE LA IMAGEN")
-    print("-"*50)
-    print(text)
-    print("-"*50)
-    trasnlator = Translator()
+class TraductorImagenes:
 
-    trasnlated_text = trasnlator.translate(text, src='en',dest='es').text
-    print("TEXTO RTRADUCIDO")
-    print("-"*50)
-    print(trasnlated_text)
-    print("-"*50)
-else:
-    print('Error')
+    def __init__(self, image):
+        self.image = image
+    def translate(self):
+        # text = pytesseract.image_to_string(Image.open('test.png'))
+        text = pytesseract.image_to_string(self.image)
+        if text:
+            print("TEXTO RECOGIDO DE LA IMAGEN")
+            print("-"*50)
+            print(text)
+            print("-"*50)
+            trasnlator = Translator()
+
+            trasnlated_text = trasnlator.translate(text, src='en',dest='es').text
+            print("TEXTO RTRADUCIDO")
+            print("-"*50)
+            print(trasnlated_text)
+            print("-"*50)
+        else:
+            print('Error')
