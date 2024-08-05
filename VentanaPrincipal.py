1f13bf75ac6b77be2689dbb77e5d5e2e03e12d60
FelipeIzRo <felipe.iz.ro.98@gmail.com>
2024-08-05 11:34:30 +0200

    Añadido el boton de parar de traducir

--- VentanaPrincipal.py
+++ VentanaPrincipal.py
@@ -9,9 +9,12 @@ class VentanaPrincipal:
         self.root.geometry('600x400')
         self.root.configure(bg='#808185')
 
-        size = (150, 50)
-        image = ImageTk.PhotoImage(Image.open('imagesInterface/button.png').resize(size))
+        
 
+        size = (150, 50)
+        image = ImageTk.PhotoImage(Image.open('ImagesInterface/button_comenzar.png').resize(size))
+        image_destroy = ImageTk.PhotoImage(Image.open('ImagesInterface/button_parar.png').resize(size))
+        
         button = tk.Button(self.root,
                           text='EmpezarTraduccion',
                           command=self.instancia_selector,
@@ -19,14 +22,22 @@ class VentanaPrincipal:
                           bg=self.root.cget('bg'),
                           bd=0,
                           image=image)
+        
+        button_destroy = tk.Button(self.root,
+                                   text='TerminarTraduccion',
+                                   command=self.stop_all_selectors,
+                                   cursor='hand2',
+                                   bg=self.root.cget('bg'),
+                                   bd=0,
+                                   image=image_destroy)
         button.pack(side=tk.BOTTOM, anchor=tk.SE, padx=1, pady=1)
+        button_destroy.pack(side=tk.BOTTOM, anchor=tk.SE, padx=1, pady=1)
 
         self.label = tk.Label(self.root, text='EmpezarTraduccion')
         self.label.pack()
         
-
+        self.selector = None
         self.root.mainloop()
-
         
     def LabelTranslated(self,text):
         self.label.pack_forget()
@@ -35,4 +46,7 @@ class VentanaPrincipal:
         print (type(text))
     
     def instancia_selector(self):
-        selector = ScreenSelector(self)
+        self.selector = ScreenSelector(self)
+
+    def stop_all_selectors(self):
+        self.selector.root.destroy()
